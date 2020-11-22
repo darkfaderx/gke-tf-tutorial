8e70f565df0b4464af19bfa03e70ab2be61f0072
darkfaderx <darkfaderx@hotmail.com>
2020-11-22 17:15:29 +1100

    Update gkecluster.tf

--- gkecluster.tf
+++ gkecluster.tf
@@ -1,6 +1,6 @@
-resource "google_container_cluster" "gke-cluster" {
+resource "google_container_cluster" "gke-cluster-1" {
   name               = "my-first-gke-cluster"
   network            = "default"
-  location           = "europe-west1-b"
+  location           = "australia-southeast1-a"
   initial_node_count = 3
 }
